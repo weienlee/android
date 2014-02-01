fce5493997296893b24d6d6226e702c436957510
Wei-En Lee <weienlee@mit.edu>
2014-02-01 15:31:58 -0500

    did the actionbar tutorial

--- helloworld/src/com/example/helloworld/MainActivity.java
+++ helloworld/src/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -20,9 +21,26 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
