3c8fae64666c02e754827b91f05e1989b786b788
Wei-En Lee <weienlee@mit.edu>
2014-09-25 01:21:00 -0400

    fixed pause/resume, added orientation data logging

--- uropdemo/gen/com/weienlee/uropdemo/R.java
+++ uropdemo/gen/com/weienlee/uropdemo/R.java
@@ -24,10 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080009;
         public static final int mybutton=0x7f080002;
         public static final int tv=0x7f080000;
         public static final int videoview=0x7f080001;
+        public static final int x_label=0x7f080003;
+        public static final int x_value=0x7f080004;
+        public static final int y_label=0x7f080005;
+        public static final int y_value=0x7f080006;
+        public static final int z_label=0x7f080007;
+        public static final int z_value=0x7f080008;
     }
     public static final class layout {
         public static final int activity_access_gyroscope=0x7f030000;
@@ -44,6 +50,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int blank=0x7f05000c;
         public static final int camera_button=0x7f050004;
         public static final int gyro_button=0x7f050008;
         public static final int hello=0x7f050006;
@@ -51,6 +58,9 @@ public final class R {
         public static final int title_activity_access_gyroscope=0x7f050007;
         public static final int title_activity_camera=0x7f050003;
         public static final int title_activity_videocamera=0x7f050005;
+        public static final int x_label=0x7f050009;
+        public static final int y_label=0x7f05000a;
+        public static final int z_label=0x7f05000b;
     }
     public static final class style {
         /** 
--- uropdemo/src/com/weienlee/uropdemo/VideocameraActivity.java
+++ uropdemo/src/com/weienlee/uropdemo/VideocameraActivity.java
@@ -1,7 +1,9 @@
 package com.weienlee.uropdemo;
 
+import java.io.FileWriter;
 import java.io.IOException;
-
+import java.text.NumberFormat;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
@@ -17,20 +19,26 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class VideocameraActivity extends Activity{
-
+public class VideocameraActivity extends Activity implements SensorEventListener{
+	private FileWriter writer;
 	private Camera myCamera;
 	private MyCameraSurfaceView myCameraSurfaceView;
 	private MediaRecorder mediaRecorder;
-
+	private FrameLayout myCameraPreview;
 	Button myButton;
 	SurfaceHolder surfaceHolder;
-
+	private TextView x_value;
+	private TextView y_value;
+	private TextView z_value;
+	private SensorManager sManager; //the Sensor Manager
+	
 	boolean recording;
 
 	/** Called when the activity is first created. */
+	@SuppressLint("NewApi")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,7 +46,17 @@ public class VideocameraActivity extends Activity{
 		recording = false;
 
 		setContentView(R.layout.main);
-
+		getActionBar().hide();
+		
+		// set up gyroscope
+        //get the TextView from the layout file
+		x_value = (TextView) findViewById(R.id.x_value);
+		y_value = (TextView) findViewById(R.id.y_value);
+		z_value = (TextView) findViewById(R.id.z_value);
+        //get a hook to the sensor service
+        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        
+        
 		//Get Camera for preview
 		myCamera = getCameraInstance();
 		if(myCamera == null){
@@ -48,24 +66,32 @@ public class VideocameraActivity extends Activity{
 		}
 
 		myCameraSurfaceView = new MyCameraSurfaceView(this, myCamera);
-		FrameLayout myCameraPreview = (FrameLayout)findViewById(R.id.videoview);
+		myCameraPreview = (FrameLayout)findViewById(R.id.videoview);
 		myCameraPreview.addView(myCameraSurfaceView);
 
 		myButton = (Button)findViewById(R.id.mybutton);
 		myButton.setOnClickListener(myButtonOnClickListener);
 	}
 
+	
+	
 	Button.OnClickListener myButtonOnClickListener = new Button.OnClickListener(){
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			if(recording){
+				// stop recording gyroscope data
+				try {
+					writer.close();
+				} catch (IOException e) {
+				}
+				
 				// stop recording and release camera
 				mediaRecorder.stop();  // stop the recording
 				releaseMediaRecorder(); // release the MediaRecorder object
 
-				//Exit after saved
-				finish();
+				myButton.setText("REC");
+				recording = false;
 			}else{
 
 				//Release Camera before MediaRecorder start
@@ -109,9 +135,9 @@ public class VideocameraActivity extends Activity{
 
 		mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
 
-		mediaRecorder.setOutputFile("/sdcard/myvideo.mp4");
+		mediaRecorder.setOutputFile("/sdcard/uropdemo.mp4");
 		mediaRecorder.setMaxDuration(60000); // Set max duration 60 sec.
-		mediaRecorder.setMaxFileSize(5000000); // Set max file size 5M
+		mediaRecorder.setMaxFileSize(50000000); // Set max file size 5M
 
 		mediaRecorder.setPreviewDisplay(myCameraSurfaceView.getHolder().getSurface());
 
@@ -127,14 +153,67 @@ public class VideocameraActivity extends Activity{
 		return true;
 
 	}
-
+	
 	@Override
 	protected void onPause() {
 		super.onPause();
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// stop recording
+		if (mediaRecorder != null) {
+			mediaRecorder.stop();
+		}
+		myButton.setText("REC");
+		recording = false;
+		
 		releaseMediaRecorder();       // if you are using MediaRecorder, release it first
 		releaseCamera();              // release the camera immediately on pause event
+		myCameraPreview.removeView(myCameraSurfaceView);
 	}
 
+    @Override
+	protected void onResume()
+	{
+		super.onResume();
+		/*register the sensor listener to listen to the gyroscope sensor, use the
+		callbacks defined in this class, and gather the sensor information as quick
+		as possible*/
+		sManager.registerListener(this, sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_UI);
+		
+		try {
+			writer = new FileWriter("/sdcard/uropdemo.txt",true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// setup camera and surface view again
+		if (myCamera == null) {
+			myCamera = getCameraInstance();
+			if(myCamera == null){
+				Toast.makeText(VideocameraActivity.this,
+						"Fail to get Camera",
+						Toast.LENGTH_LONG).show();
+			}
+
+			myCameraSurfaceView = new MyCameraSurfaceView(this, myCamera);
+			myCameraPreview.addView(myCameraSurfaceView);
+		}
+		
+	}
+	
+  	@Override
+  	protected void onStop()
+  	{
+  		//unregister the sensor listener
+  		sManager.unregisterListener(this);
+  		super.onStop();
+  	}
+    
 	private void releaseMediaRecorder(){
 		if (mediaRecorder != null) {
 			mediaRecorder.reset();   // clear recorder configuration
@@ -211,9 +290,55 @@ public class VideocameraActivity extends Activity{
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
-
+			/*this.getHolder().removeCallback(this);
+			mCamera.stopPreview();
+			mCamera.release();*/
 		}
 	}
 
+	
+	// gyroscope sensor
+	
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1)
+	{
+		//Do nothing.
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		//if sensor is unreliable, return void
+		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+		{
+			return;
+		}
+		//else it will output the Roll, Pitch and Yawn values
+		float x = event.values[2];
+		float y = event.values[1];
+		float z = event.values[0];
+
+		// round numbers
+		NumberFormat formatter = NumberFormat.getNumberInstance();
+		formatter.setMinimumFractionDigits(2);
+		formatter.setMaximumFractionDigits(2);
+		/*tv.setText("Orientation X (Roll) :"+ Float.toString(event.values[2]) +"\n"+
+				   "Orientation Y (Pitch) :"+ Float.toString(event.values[1]) +"\n"+
+				   "Orientation Z (Yaw) :"+ Float.toString(event.values[0]));
+		 */
+		String x_str = formatter.format(x);
+		String y_str = formatter.format(y);
+		String z_str = formatter.format(z);
+		
+		x_value.setText(x_str);
+		y_value.setText(y_str);
+		z_value.setText(z_str);
+		try {
+			writer.write(x_str+", "+y_str+", "+z_str+"\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
